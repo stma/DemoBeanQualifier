fdfea34df64c407f730751e08a0a9179a3623137
Matyi <steinermatyas@gmail.com>
2023-02-13 12:05:34 +0100

    remove magic number

--- src/main/java/com/example/demobeanqualyfier/BookPaddingFactory.java
+++ src/main/java/com/example/demobeanqualyfier/BookPaddingFactory.java
@@ -9,24 +9,27 @@ import java.util.List;
 public class BookPaddingFactory {
 
     private final int minColumnSize;
+    private final int sidePadding;
 
-    public BookPaddingFactory(@Value("${min_column_size}") int mcs) {
+    public BookPaddingFactory(@Value("${min_column_size}") int mcs, @Value("${side_padding}") int sp) {
         this.minColumnSize = mcs;
+        this.sidePadding = sp;
     }
     public BookPadding create(Book book) {
-        int titleSize = Math.max(book.title().length() + 2, minColumnSize);
-        int authorSize = Math.max(book.author().length() + 2, minColumnSize);
+        int titleSize = Math.max(book.title().length() + 2 * this.sidePadding, minColumnSize);
+        int authorSize = Math.max(book.author().length() + 2 * this.sidePadding, minColumnSize);
         int lineSize = titleSize + authorSize + 3;
 
         return new BookPadding(titleSize, authorSize, lineSize);
     }
+
     public BookPadding create(List<Book> bs) {
         int titleSize = minColumnSize;
         int authorSize = minColumnSize;
 
         for (Book book: bs) {
-            titleSize = Math.max(book.title().length() + 2, titleSize);
-            authorSize = Math.max(book.author().length() + 2, authorSize);
+            titleSize = Math.max(book.title().length() + 2 * this.sidePadding, titleSize);
+            authorSize = Math.max(book.author().length() + 2 * this.sidePadding, authorSize);
         }
 
         return new BookPadding(titleSize, authorSize, titleSize + authorSize + 3);
